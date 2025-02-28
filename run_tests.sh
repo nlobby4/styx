a3f282a0e62556fd288da33f4aeb0951c92cd6e8
Der Breski <atenne@web.de>
2025-03-01 00:30:17 +0100

    coverage now works

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
 ./build/Tests
-# LOCATION=coverage
-#cd src
-#find -name '*.c' -exec cp -t ../$LOCATION {} +
-#find -name '*.gcno' -exec cp -t ../$LOCATION {} +
-#find -name '*.gcda' -exec cp -t ../$LOCATION {} +
-#cd ..
-#cd build/CMakeFiles/Tests.dir/tests
-#find -name '*.c' -exec cp -t ../../../../$LOCATION {} +
-#find -name '*.gcno' -exec cp -t ../../../../$LOCATION {} +
-#find -name '*.gcda' -exec cp -t ../../../../$LOCATION {} +
-#cd ../../../../$LOCATION
-#rm main.c
-#find -name '*.c' -exec gcov -bf {} \;
-
-#gcov -o build/CMakeFiles/Tests.dir/src $(find src -name '*.c')
+gcovr --root ../styx -j 10 --html-details -o coverage/ --exclude 'tests/*' --exclude 'src/main.c'
